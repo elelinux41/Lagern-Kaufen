fac9858f3e8b98c91e60c87e479065fd10db6213
elelinux41 <78487921+elelinux41@users.noreply.github.com>
2023-06-26 07:28:06 +0200

    Logout funktionert jetzt

--- StoreBuy/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ StoreBuy/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -14,10 +14,10 @@ namespace StoreBuy.Areas.Identity.Pages.Account
 {
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<Costumer> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<Costumer> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
